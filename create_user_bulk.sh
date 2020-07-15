bd2d8eb69ec51b72f042a940b20f59ab00b06740
mlrx <[MASKED]>
2020-07-15 14:42:48 +0200

    ajout d'um commentaire pour les arguments à passer au script

--- create_user_bulk.sh
+++ create_user_bulk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+ARG1: fichier contenant uniquement le user de chaq adresse
+ARG2: domaine
+
 while IFS= read -r login; do
 	echo "Création de la boite email de $login" >> ~/create_user_bulk.log
 	postfixadmin-cli mailbox add ${login}@${2} --password ${login}123 --password2 ${login}123 --name $login --quota 16 >> ~/create_user_bulk.log

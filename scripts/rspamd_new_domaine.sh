1f76be9ece96d8f0c8dc4b341b6a9d41fa6d6a4a
vlp <thomas@criscione.fr>
2021-02-12 12:10:38 +0100

    script rspamd new domaine

---
+++ scripts/rspamd_new_domaine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$dn=$1
+
+echo "$dn fdn" >> /etc/rspamd/dkim_selectors.map
+echo "$dn /var/lib/rspamd/dkim/fdn.$dn.key" >> /etc/rspamd/dkim_paths.map
+rspamadm dkim_keygen -b 2048 -s fdn -d $dn -k /var/lib/rspamd/dkim/fdn.$dn.key > /var/lib/rspamd/dkim/fdn.$dn.fr.pub
+chmod u=rw,g=r,o= /var/lib/rspamd/dkim/*
+chown _rspamd /var/lib/rspamd/dkim/*

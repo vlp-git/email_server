b4aebf4d44e341ff284f413b9218920387759eb0
vlp <vlp@taslin.fdn.fr>
2021-02-12 12:37:22 +0100

    update

--- scripts/rspamd_new_domaine.sh
+++ scripts/rspamd_new_domaine.sh
@@ -4,7 +4,8 @@ dn=$1
 
 echo "$dn fdn" >> /etc/rspamd/dkim_selectors.map
 echo "$dn /var/lib/rspamd/dkim/\$selector.$dn.key" >> /etc/rspamd/dkim_paths.map
-rspamadm dkim_keygen -b 2048 -s fdn -d $dn -k /var/lib/rspamd/dkim/fdn.$dn.key > /var/lib/rspamd/dkim/fdn.$dn.fr.pub
+rspamadm dkim_keygen -b 2048 -s fdn -d $dn -k /var/lib/rspamd/dkim/fdn.$dn.key > /var/lib/rspamd/dkim/fdn.$dn.pub
 chmod u=rw,g=r,o= /var/lib/rspamd/dkim/*
 chown _rspamd /var/lib/rspamd/dkim/*
 systemctl reload rspamd
+cat /var/lib/rspamd/dkim/fdn.$dn.pub
